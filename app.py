f5a4aa232cb653512a2bf510cf939a787e4d20ea
wstays <debbie.hahm@gmail.com>
2019-11-19 19:38:07 -0500

    fixed listing form bugs

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask import (Flask, render_template, make_response, url_for, request,
 from werkzeug import secure_filename
 app = Flask(__name__)
 
-import sys, os, random, listing
+import sys, os, random
+import functions
 
 app.secret_key = 'your secret here'
 # replace that with a random key
@@ -20,113 +21,31 @@ app.config['TRAP_BAD_REQUEST_ERRORS'] = True
 def index():
     return render_template('home.html', page_title='NICOLE!')
 
-@app.route('/listing')
+@app.route('/listing/', methods=["GET"])
 def listing():
-    conn = listing.getConn('wstays_db')
-    if (request.method == 'POST'):
-        form = request.form
-        listing.insertListing(conn, form.get("user"), form.get("street1"),
-        form.get("street2"), form.get("city"), form.get("state"),
-        form.get("zip"), form.get("country"), form.get("maxguest"), 
-        form.get("start"), form.get("end"))
+    conn = functions.getConn('wstays_db')
 
-        return render_template('listingconfirmation.html', form=form)
-    else:
-        # if 'bnumber' in session:
-        # uncomment out code once login is implemented
-        # bnumber = session['bnumber']
-        bnumber = "B20856852"   
-        user = listing.getUser(conn, bnumber)
-        return render_template('insert.html', user=user)
+    # if 'bnumber' in session:
+    # uncomment out code once login is implemented
+    # bnumber = session['bnumber']
+    bnumber = "B20856852"   
+    user = functions.getUser(conn, bnumber)
+    return render_template('listingform.html', user=user)
 
-        # else:
-        #     flash('you are not logged in. Please login or join')
-        #     return redirect(url_for('index'))
-        
+    # else:
+    #     flash('you are not logged in. Please login or join')
+    #     return redirect(url_for('index'))
 
-@app.route('/insert/', methods=["GET", "POST"])
-def insert():
-    conn = lookup.getConn("nli2_db")
-    message=''
-    if request.method == 'POST':
-        bnumber = request.form['bnumber']
-        exists = profile.checkUser(conn,bnumber)
-        if exists:
-            message = 'error: user exists; User with bnumber: %s is already in database' %bnumber
-        else:
-            email = request.form['email']
-            name = request.form['name']
-            phonenum = request.form['phonenum']
-            profile.insertUser(conn,bnumber,email,name,phonenum)
-            message = 'User %s inserted.' %name
-        flash(message)
-        return redirect( url_for('update', bnumber=bnumber) )
-    else:
-        return render_template('insert.html', title='Insert A User')
+@app.route('/listingecho/', methods=['POST'])
+def listingecho():
+    conn = functions.getConn('wstays_db')
+    form = request.form
+    functions.insertListing(conn, form.get("user"), form.get("street1"),
+    form.get("street2"), form.get("city"), form.get("state"),
+    form.get("zip"), form.get("country"), form.get("maxguest"), 
+    form.get("start"), form.get("end"))
 
-@app.route('/update/<tt>', methods=["GET", "POST"])
-def update(tt):
-    conn = lookup.getConn("wmdb")
-    if request.method == 'GET':
-        movie = lookup.getMovie(conn,tt)
-        director = ''
-        if movie[3] is None:
-            director = 'None Specified'
-        else:
-            director = lookup.getDirector(conn,movie[3])
-        return render_template('update.html', movie=movie, director=director)
-    else:
-        if request.form['submit'] == 'update':
-            new_tt = request.form['movie-tt']
-            exist = lookup.checkMovie(conn,new_tt)
-            director_id = request.form['movie-director']
-            valid_nm = lookup.checkDirector(conn,director_id)
-            if exist and new_tt != tt:
-                flash('Movie already exists')
-                return redirect( url_for('update', tt=tt) )
-            else:
-                if valid_nm or director_id is None:
-                    lookup.updateMovie(conn,new_tt,request.form['movie-title'],
-                        request.form['movie-release'],request.form['movie-director'],tt)
-                    flash('Movie (%s) was successfully updated' %request.form['movie-title'])
-                    return redirect( url_for('update', tt=new_tt) )
-                else:
-                    flash('Director ID is invalid: %s' %director_id)
-                    return redirect( url_for('update', tt=tt) )
-        else:
-            lookup.deleteMovie(conn,tt)
-            flash('Movie (%s) was deleted successfully' %tt)
-            return redirect(url_for('index'))
-
-@app.route('/form/', methods=["GET", "POST"])
-def greet():
-    if request.method == 'GET':
-        return render_template('form.html', page_title='FORM')
-    else:
-        try:
-            return render_template('form.html', page_title='FORM')
-
-        except Exception as err:
-            flash('form submission error'+str(err))
-            return redirect( url_for('index') )
-
-@app.route('/formecho/', methods=['GET','POST'])
-def formecho():
-    if request.method == 'GET':
-        return render_template('form_data.html',
-                               method=request.method,
-                               form_data=request.args,
-                               page_title='ECHO')
-    elif request.method == 'POST':
-        return render_template('form_data.html',
-                               method=request.method,
-                               form_data=request.form,
-                               page_title='ECHO')
-    else:
-        return render_template('form_data.html',
-                               method=request.method,
-                               form_data={},
-                               page_title='ECHO')
+    return render_template('listingconfirmation.html', form=form)
 
 if __name__ == '__main__':
 
