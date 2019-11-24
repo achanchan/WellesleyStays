5add18ecc1ff7395bee28d906056bc7eb8e63219
Amy <achan@cs.wellesley.edu>
2019-11-24 15:02:36 -0500

    working on search form

--- functions.py
+++ functions.py
@@ -20,14 +20,6 @@ def getUser(conn, bnumber):
 def insertListing(conn, bnumber, street1, street2, city, state, zipcode, country, maxguest, start, end):
     '''Inserts a listing and the corresponding availability'''
     curs = dbi.cursor(conn)
-<<<<<<< HEAD
-    curs.execute('''insert into place(bnumber, city, country, street1, street2,
-                    state, maxguest, postalcode) values(%s, %s, %s, %s, %s, %s, %s, %s)''',
-                [bnumber, city, country, street1, street2, state, maxguest, zipcode])
-    pid = curs.lastrowid
-    curs.execute('''insert into availability(pid, start, end) values(%s, %s, %s)''',
-                [pid, start, end])
-=======
     curs.execute('''insert into places(bnumber, city, country, street1, street2,
                     state, maxguest, postalcode) values(%s, %s, %s, %s, %s, %s)''',
                 [bnumber, street1, street2, city, state, zipcode, country, maxguest])
@@ -35,16 +27,12 @@ def insertListing(conn, bnumber, street1, street2, city, state, zipcode, country
     curs.execute('''insert into availablity(pid, start, end) values(%s, %s, %s)''',
                 [pid, start, end])
                 
->>>>>>> c062e5996d22878163715edfc49de2c5205156e4
 def allListings(conn):
     curs = dbi.dictCursor(conn)
     curs.execute('''select * from place''')
     return curs.fetchall()
-<<<<<<< HEAD
 
 def searchPlace(conn, search, guests):
     curs = dbi.dictCursor(conn)
-    curs.execute('''select * from place where city like %s and maxguest=%s''', [search,guest])
+    curs.execute('''select * from place where city like %s and maxguest=%s''', ['%'+search+'%',guests])
     return curs.fetchall()
-=======
->>>>>>> c062e5996d22878163715edfc49de2c5205156e4
