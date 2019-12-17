69ae1af001ad0b1e9581e46412f4d79fac61cba5
wstays <debbie.hahm@gmail.com>
2019-12-17 12:04:16 -0500

    change database to wstays_db

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ app.config['CAS_AFTER_LOGIN'] = 'index'
 
 # This gets us better error messages for certain common request errors
 app.config['TRAP_BAD_REQUEST_ERRORS'] = True
-db = "achan_db"
+db = "wstays_db"
 
 @app.route('/')
 def index():
