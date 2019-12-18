47eab73119430bcee633c6597dad49d3760e2cbf
wstays <debbie.hahm@gmail.com>
2019-12-17 22:29:16 -0500

    progressive enhancement

--- static/plain.js
+++ static/plain.js
@@ -6,7 +6,8 @@ function processDelete(resp) {
     $("[data-aid='"+aid+"']").remove();
 };
 
-$("#availabilities").on('click', '.deleteAvailability', function(event){
+$("#availabilities").on('click', '.deleteAvailabilityAjax', function(event){
+    if (!progressive_on) return;
     if (event.target != this) return;
     var aid = $(this).closest("[data-aid").attr("data-aid");
 
